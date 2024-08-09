5628de77b1580113076000eaf32f542c8db7a46c
leeseungtack <dltmdxor159@naver.com>
2024-08-09 15:12:12 +0900

    - 1.0.7 - colorPrimary 명칭 변경

--- ax-permission/build.gradle.kts
+++ ax-permission/build.gradle.kts
@@ -62,14 +62,14 @@ afterEvaluate {
 
                 groupId = "com.github.mojise.ax-permission" // 깃허브 이름 예제
                 artifactId = "Ax-Permission" // 공개할 라이브러리의 이름 예제
-                version = "1.0.6" // 버전 예제
+                version = "1.0.7" // 버전 예제
             }
             create<MavenPublication>("debug") {
                 from(components["debug"])
 
                 groupId = "com.github.mojise.ax-permission" // 깃허브 이름 예제
                 artifactId = "Ax-Permission" // 공개할 라이브러리의 이름 예제
-                version = "1.0.6" // 버전 예제
+                version = "1.0.7" // 버전 예제
             }
         }
     }
