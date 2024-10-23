6a159c5b8000b6be37cd1bcb5031e059e82c2940
leeseungtack <dltmdxor159@naver.com>
2024-10-23 17:22:57 +0900

    - 1.1.6 - WIFI 설정 Access 권한 추가

--- ax-permission/src/main/java/kr/co/permission/ax_permission/util/AxPermissionSettings.kt
+++ ax-permission/src/main/java/kr/co/permission/ax_permission/util/AxPermissionSettings.kt
@@ -292,12 +292,13 @@ class AxPermissionSettings {
             false,
             "access",0 // 접근 권한
         )
-        /*perMap[Manifest.permission.ACCESS_COARSE_LOCATION] = AxPermissionModel(
-            "대략적인 위치 정보",
-            "이 권한은 사용자의 대략적인 위치를 얻기 위해 필요합니다.",
-            Manifest.permission.ACCESS_COARSE_LOCATION,
+        perMap[Manifest.permission.NEARBY_WIFI_DEVICES] = AxPermissionModel(
+            "WIFI 설정",
+            "Android 13(API 33) 이상에서 사용되며, Wi-Fi 네트워크와 관련된 기능(예: Wi-Fi 스캔, 네트워크 연결, 네트워크 제안 등)을 사용하기 위해 필요한 권한 입니다.",
+            Manifest.permission.ACCESS_FINE_LOCATION,
             false,
-            "access" // 접근 권한
-        )*/
+            "access",0 // 접근 권한
+        )
+
     }
 }
