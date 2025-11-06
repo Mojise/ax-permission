2ba68f897fc653e55a2e46a08085d3d5aeae425a
seongrok cho <jsl663663@gmail.com>
2025-11-06 16:24:53 +0900

    temp10

---
+++ ax-permission/src/main/java/com/ax/library/ax_permission/datastore/AxPermissionPrefs.kt
@@ -0,0 +1,46 @@
+package com.ax.library.ax_permission.datastore
+
+import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.ax.library.ax_permission.model.Permission
+import com.ax.library.ax_permission.permission.PermissionChecker
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.runBlocking
+
+internal object AxPermissionPrefs {
+
+    object Key {
+
+    }
+
+    @JvmSynthetic
+    internal fun foo(permission: Permission.Runtime, result: PermissionChecker.Result.Runtime) {
+
+
+
+    }
+
+    private fun getPermissionPreferencesKey(permission: Permission.Runtime): String {
+        return permission.manifestPermissions.first()
+    }
+
+    private lateinit var context: Context
+
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("com.ax.library.ax_permission.prefs")
+
+    private fun <T> runBlockingPrefsGet(key: Preferences. Key<T>): T? =
+        runBlocking { context.dataStore.data.firstOrNull()?.get(key) }
+
+    private fun <T> runBlockingPrefsSet(key: Preferences. Key<T>, value: T): Unit =
+        runBlocking { context.dataStore.edit { preferences -> preferences[key] = value } }
+
+    @JvmSynthetic
+    internal fun setContext(application: Application) {
+        context = application.applicationContext
+    }
+}
--- ax-permission/src/main/java/com/ax/library/ax_permission/permission/PermissionChecker.kt
+++ ax-permission/src/main/java/com/ax/library/ax_permission/permission/PermissionChecker.kt
@@ -1,30 +1,37 @@
 package com.ax.library.ax_permission.permission
 
+import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.PowerManager
 import android.provider.Settings
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import com.ax.library.ax_permission.model.Permission
 
 internal object PermissionChecker {
 
+    private const val TAG = "PermissionChecker"
+
     /**
      * 권한이 허용되었는지 확인
      */
-    fun check(context: Context, permission: Permission): Boolean {
+    @JvmSynthetic
+    fun check(activity: Activity, permission: Permission): Result {
         return when (permission) {
-            is Permission.Special -> checkSpecialPermission(context, permission)
-            is Permission.Runtime -> checkRuntimePermission(context, permission)
+            is Permission.Special -> checkSpecialPermission(activity, permission)
+            is Permission.Runtime -> checkRuntimePermission(activity, permission)
         }
     }
 
     /**
      * 특별 권한 체크
      */
-    private fun checkSpecialPermission(context: Context, permission: Permission.Special): Boolean {
-        return when (permission.settingsAction) {
+    @JvmSynthetic
+    fun checkSpecialPermission(context: Context, permission: Permission.Special): Result.Special {
+        val isGranted = when (permission.settingsAction) {
             Settings.ACTION_MANAGE_OVERLAY_PERMISSION ->
                 Settings.canDrawOverlays(context)
 
@@ -38,23 +45,86 @@ internal object PermissionChecker {
 
             else -> false
         }
+        return if (isGranted) Result.Special.Granted else Result.Special.Denied
     }
 
     /**
      * 런타임 권한 체크
      * manifestPermissions에 포함된 모든 권한이 허용되어야 true 반환
      */
-    private fun checkRuntimePermission(context: Context, permission: Permission.Runtime): Boolean {
-        val permissions = permission.manifestPermissions
+    @JvmSynthetic
+    fun checkRuntimePermission(activity: Activity, permission: Permission.Runtime): Result.Runtime {
+        val manifestPermissions = permission.manifestPermissions
 
         // manifestPermissions가 비어있으면 (해당 OS 버전에서 불필요) true 반환
-        if (permissions.isEmpty()) {
-            return true
+        if (manifestPermissions.isEmpty()) {
+            return Result.Runtime.Granted
         }
 
+
+        Log.e(TAG, buildString {
+            appendLine("checkRuntimePermission() :: $permission")
+
+            manifestPermissions.forEach {
+                val granted = ContextCompat.checkSelfPermission(activity, it) == PackageManager.PERMISSION_GRANTED
+                val showRational = ActivityCompat.shouldShowRequestPermissionRationale(activity, it)
+                appendLine("    [${it.split(".").last()}]")
+                appendLine("      - granted=$granted")
+                appendLine("      - showRational=$showRational")
+            }
+        })
+
         // 모든 권한이 허용되어야 true
-        return permissions.all { manifestPermission ->
-            ContextCompat.checkSelfPermission(context, manifestPermission) == PackageManager.PERMISSION_GRANTED
+        val isAllGranted = manifestPermissions.all { manifestPermission ->
+            ContextCompat.checkSelfPermission(activity, manifestPermission) == PackageManager.PERMISSION_GRANTED
+        }
+        return if (isAllGranted) Result.Runtime.Granted else Result.Runtime.DeniedCanTryOnce
+    }
+
+    /**
+     * 권한 요청 결과
+     */
+    internal sealed interface Result {
+
+        val isGranted: Boolean
+
+        val isDenied: Boolean
+            get() = isGranted.not()
+
+        /**
+         * 특별 권한 요청 결과
+         */
+        enum class Special : Result {
+
+            /** 승인됨 */
+            Granted,
+
+            /** 거부됨 */
+            Denied;
+
+            override val isGranted: Boolean
+                get() = this == Granted
+        }
+
+        /**
+         * 런타임 권한 요청 결과
+         */
+        enum class Runtime : Result {
+
+            /** 아직 한 번도 권한 요청을 하지 않은 상태 (최초의 상태) */
+            Initial,
+
+            /** 거부됨 (한 번 더 요청 가능) */
+            DeniedCanTryOnce,
+
+            /** 영구 거부됨 */
+            DeniedPermanently,
+
+            /** 승인됨 */
+            Granted;
+
+            override val isGranted: Boolean
+                get() = this == Granted
         }
     }
 }
